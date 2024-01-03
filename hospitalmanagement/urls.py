2f8ed577edb725a2a6f913ccace72a1558ddeb0d
Chaimaa Bourajli <cbourajl@student.1337.ma>
2024-01-03 18:03:40 +0100

    superadminclick & staffclick

--- hospitalmanagement/urls.py
+++ hospitalmanagement/urls.py
@@ -1,19 +1,8 @@
-"""
-
-
-
-"""
-
-
-
-
 from django.contrib import admin
 from django.urls import path
 from hospital import views
 from django.contrib.auth.views import LoginView,LogoutView
 
-
-#-------------FOR ADMIN RELATED URLS
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home_view,name=''),
@@ -26,6 +15,8 @@ urlpatterns = [
     path('adminclick', views.adminclick_view),
     path('doctorclick', views.doctorclick_view),
     path('patientclick', views.patientclick_view),
+    path('superadminclick',views.superadminclick_view ),
+    path('staffclick', views.staffclick_view),
 
     path('adminsignup', views.admin_signup_view),
     path('doctorsignup', views.doctor_signup_view,name='doctorsignup'),
